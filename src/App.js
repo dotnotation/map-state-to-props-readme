92e29539c924b98561d2e44d1522e7fc5285c86b
dotnotation <dotinmotionproductions@gmail.com>
2022-02-20 18:29:28 -0500

    commit tree 4c03e6b2e41742094c99fb8bb45524a5be2404a2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux"; /* code change */
+import "./App.css";
 
 class App extends Component {
-	handleOnClick = () => {
-		this.props.store.dispatch({
-			type: 'INCREASE_COUNT',
-		});
-	}
 
-	render() {
-		return (
-			<div className="App">
-				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.store.getState().clicks}</p>
-			</div>
-		);
-	}
+    handleOnClick = () => {
+        this.props.dispatch({
+          type: "INCREASE_COUNT",
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.handleOnClick}>Click</button>
+                <p>{this.props.clicks}</p> {/* code change */}
+            </div>
+        );
+    }
+	// state is no longer being directly passed down to App because of Provider
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return { clicks: state.clicks };
+};
+
+export default connect(mapStateToProps)(App);
+// The connect function is taking care of task 1; it is synced up to our store, listening to each change in the state that occurs. When a change occurs, it calls a function that we write called mapStateToProps(), and in mapStateToProps() we specify exactly which slice of the state we want to provide to our component. Here, we want to provide state.clicks, and allow our component to have access to them through a prop called clicks. We are then able to render the number of clicks in our render method using this.props.clicks. 
